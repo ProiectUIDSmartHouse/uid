23934ec310378d6b5d724b81fe6e79302e58f440
crisanoana51 <crisanoana51@yahoo.com>
2024-01-13 17:17:39 +0200

    design naspa da sa nu stric ceva

--- uidproject/app/src/main/java/uid/project/TemperatureActivity.kt
+++ uidproject/app/src/main/java/uid/project/TemperatureActivity.kt
@@ -4,12 +4,49 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 
 class TemperatureActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.temperature)
+
+        val temperatureSlider: SeekBar = findViewById(R.id.temperatureSlider)
+        val temperatureDisplay: TextView = findViewById(R.id.temperatureDisplay)
+        val onOffButton: Button = findViewById(R.id.onOffButton)
+
+        temperatureSlider.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+
+                temperatureDisplay.text = "Temperature: ${progress}°C"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+
+                val selectedTemperature = seekBar.progress
+                val message = "Temperature is set on $selectedTemperature degrees"
+                showToast(message)
+
+            }
+        })
+        var isTurnedOn = false
+        onOffButton.setOnClickListener {
+            if (isTurnedOn) {
+                onOffButton.text = "TURN ON"
+            } else {
+                onOffButton.text = "TURN OFF"
+            }
+            isTurnedOn = !isTurnedOn
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -26,5 +63,8 @@ class TemperatureActivity : ComponentActivity() {
             }
             else -> return super.onOptionsItemSelected(item)
         }
+    }
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
