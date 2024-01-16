3f05231c144e7129a90a30e0c50230047dccc3bc
Bodogaestefan <stef1234bodog@gmail.com>
2024-01-16 20:37:08 +0200

    notifications

--- uidproject/app/src/main/java/uid/project/NotificationsActivity.kt
+++ uidproject/app/src/main/java/uid/project/NotificationsActivity.kt
@@ -1,9 +1,9 @@
 package uid.project
 
-import android.content.Intent
+import android.app.AlertDialog
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 
 class NotificationsActivity : ComponentActivity() {
@@ -11,21 +11,22 @@ class NotificationsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.notifications)
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.home_menu, menu)
-        return true
-    }
+        val btnAskForHelp = findViewById<Button>(R.id.btnAskForHelp)
+        val btnEmergency = findViewById<Button>(R.id.btnEmergency)
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.back_home -> {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                return true
-            }
-            else -> return super.onOptionsItemSelected(item)
+        btnAskForHelp.setOnClickListener {
+            showPopup("Help notification was sent!")
         }
+
+        btnEmergency.setOnClickListener {
+            showPopup("Emergency reported, all family members were notified")
+        }
+    }
+    private fun showPopup(message: String) {
+        AlertDialog.Builder(this)
+            .setMessage(message)
+            .setPositiveButton("OK", null)
+            .show()
     }
 }
